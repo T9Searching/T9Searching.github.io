9ca32d1823bf64ee0956fecd781f4cc10fbc4994
Johnny Le <johnnyxdblack@gmail.com>
2019-02-25 12:37:57 +0000

    Update: CSS changes to footer

---
+++ src/main.js
@@ -0,0 +1,7 @@
+console.log("Main.js: Successfully loaded");
+
+var data {
+  download: 'https://mega.nz/#!Z5F2waIJ!vk9QRYgUABeABtyCBthrgZ0Bj0NwGdvUga_BE8ap84k',
+  blog: 'https://jilebantino.wixsite.com/team9-searching',
+  johnny: 'https://johnnyleteam9searching.wordpress.com/',
+};
